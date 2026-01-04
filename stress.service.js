a787c885eb2d9e3ce87c3fc476928c1425a413b1
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2026-01-05 01:04:09 +0530

    feat: implement API gateway and stress service with clustering support

--- stress.service.js
+++ stress.service.js
@@ -1,14 +1,39 @@
 const express = require("express");
 const morgan = require("morgan");
+const os = require("os");
+const cluster = require("cluster");
 
-const app = express();
-app.use(morgan("dev"));
+const numCPUs = os.cpus().length;
 
-app.get("/", (req, res) => {
-  for (let i = 0; i < 10000000000; i++); // Simulate heavy work
-  res.send("This was fast!");
-});
+if (cluster.isMaster) {
+  console.log(`Master process ${process.pid} is running`);
+  console.log(`Starting ${numCPUs} worker processes...`);
 
-app.listen(3002, () => {
-  console.log("Server is running on http://localhost:3002");
-});
+  // Fork workers for each CPU core
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  // Handle worker exit
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(
+      `Worker ${worker.process.pid} died (${signal || code}). Restarting...`
+    );
+    cluster.fork();
+  });
+} else {
+  // Worker processes
+  const app = express();
+  app.use(morgan("dev"));
+
+  app.get("/", (req, res) => {
+    for (let i = 0; i < 10000000000; i++); // Simulate heavy work
+    res.send("This was fast!");
+  });
+
+  app.listen(3002, () => {
+    console.log(
+      `Worker process ${process.pid} is running on http://localhost:3002`
+    );
+  });
+}
