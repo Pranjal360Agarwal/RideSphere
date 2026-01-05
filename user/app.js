f3986f80e8931916398e4978ca6683635cfb090a
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2026-01-05 08:46:49 +0530

    feat: add cookie-parser middleware and update package dependencies

--- user/app.js
+++ user/app.js
@@ -1,6 +1,15 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const express = require("express");
 const app = express();
 const userRoutes = require("./routes/user.routes");
+
+const cookieParser = require("cookie-parser");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/", userRoutes);
 
--- user/controllers/user.controller.js
+++ user/controllers/user.controller.js
@@ -18,6 +18,8 @@ module.exports.registerUser = async (req, res) => {
       expiresIn: "1h",
     });
 
+    res.cookie("token", token);
+
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
     res.status(500).json({ message: "Server error", error });
