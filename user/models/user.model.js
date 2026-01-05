47473238587bfcb2e6f78add27985013a2b0daa4
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2026-01-05 09:10:45 +0530

    feat: add blacklist token model and update user model with required fields

--- user/models/user.model.js
+++ user/models/user.model.js
@@ -8,10 +8,13 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     unique: true,
+    required: true,
   },
   password: {
     type: String,
     required: true,
+    select: false,
   },
 });
+
 module.exports = mongoose.model("user", userSchema);
