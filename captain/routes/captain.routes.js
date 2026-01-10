8c4a9c4e588b4e59edb0596e807a2103b52015bb
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2026-01-10 13:06:44 +0530

    Add: Completed captain microservice with the RabbitMQ

--- captain/routes/captain.routes.js
+++ captain/routes/captain.routes.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/captain.controller");
+const captainController = require("../controllers/captain.controller");
 const authMiddleware = require("../middleware/authMiddleWare");
 
-router.post("/register", userController.register);
-router.post("/login", userController.login);
-router.get("/logout", userController.logout);
-router.get("/profile", authMiddleware.captainAuth, userController.profile);
+router.post("/register", captainController.register);
+router.post("/login", captainController.login);
+router.get("/logout", captainController.logout);
+router.get("/profile", authMiddleware.captainAuth, captainController.profile);
 router.patch(
-  "/toggle-availability",
-  authMiddleware.captainAuth,
-  userController.toggleAvailability
+    "/toggle-availability",
+    authMiddleware.captainAuth,
+    captainController.toggleAvailability
+);
+router.get(
+    "/new-ride",
+    authMiddleware.captainAuth,
+    captainController.waitForNewRide
 );
 
 module.exports = router;
