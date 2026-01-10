07f75654fab6b07419f327d2f2bac4c60c07e3ad
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2026-01-10 13:02:44 +0530

    Add: Completed the user microservice with the RabbitMQ

--- user/app.js
+++ user/app.js
@@ -1,13 +1,14 @@
 const dotenv = require("dotenv");
 dotenv.config();
-
 const express = require("express");
 const app = express();
-const { connect } = require("./db/db");
+const connect = require("./db/db");
 connect();
 const userRoutes = require("./routes/user.routes");
-
 const cookieParser = require("cookie-parser");
+const rabbitMq = require("./service/rabbit");
+
+rabbitMq.connect();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
